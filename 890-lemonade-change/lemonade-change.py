4729cac1436d53867a5d395f876dd0092047a862
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-07-03 22:40:37 -0400

    Time: 7 ms (32.30%) | Memory: 22 MB (33.64%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.py
@@ -0,0 +1,33 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        
+        change = {5:0, 10:0}
+
+        for bill in bills: 
+            if bill == 5: 
+                change[bill] += 1 
+
+            elif bill == 10: 
+                if change[5] < 1:
+                    return False
+                
+                change[5] -= 1 
+                change[bill] += 1 
+
+
+            elif bill == 20: 
+                if change[5] < 1:
+                    return False 
+
+                elif change[10] > 0: 
+                    change[10] -= 1 
+                    change[5] -= 1 
+
+                elif change[5] >= 3:
+                    change[5] -= 3 
+                else: 
+                    return False 
+
+        return True 
+
+                    
