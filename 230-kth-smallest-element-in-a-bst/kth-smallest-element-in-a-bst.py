9e6d63fbfefa71923d0bf7d3faa62fb64ffab4af
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-07-09 16:07:07 -0400

    Time: 0 ms (100.00%) | Memory: 20.9 MB (93.31%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -6,24 +6,47 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        
-        stack = []
-
-        #iterative dfs 
+         
         #recursive dfs inorder - store it into an array, and then return k -1  
+        ordered_nums = [] 
+
+        def inorder(root):
+            if not root:
+                return 
+
+            inorder(root.left)
+
+            #visit 
+            ordered_nums.append(root.val)
+
+            inorder(root.right) 
+
+            return 
+
+        inorder(root)
+        return ordered_nums[k-1]
+
+
         #recursive dfs inorder - keep count and return correct 
 
-        curr = root
 
-        while stack or curr:
-            while curr:
-                stack.append(curr)
-                curr = curr.left 
 
-            curr = stack.pop()
 
-            k -= 1 
-            if k == 0:
-                return curr.val
 
-            curr = curr.right 
+        #ITERATIVE DFS SOLUTION 
+        #stack = []
+
+        # curr = root
+
+        # while stack or curr:
+        #     while curr:
+        #         stack.append(curr)
+        #         curr = curr.left 
+
+        #     curr = stack.pop()
+
+        #     k -= 1 
+        #     if k == 0:
+        #         return curr.val
+
+        #     curr = curr.right 
