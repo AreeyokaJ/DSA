cb177c3dac99b7df5e029ef949d658e0b5cc750f
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-07-26 22:33:20 -0400

    Time: 43 ms (31.29%) | Memory: 19.8 MB (6.84%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,20 +1,16 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        
-
         rows = len(heights)
         cols = len(heights[0])
-
-        atl = set()
         pac = set()
+        atl = set()
 
-
-        def dfs(r, c, visit, prev): 
-            if (min(r, c) < 0 or r == rows or c == cols
-                or heights[r][c] < prev or (r, c) in visit
-            ):
-                return 
+        def dfs(r, c, visit, prev):
             
+            #base cases 
+            if (min(r, c) < 0 or r == rows or c == cols or 
+                (r, c) in visit or heights[r][c] < prev): 
+                return 
             
             visit.add((r, c))
             dfs(r + 1, c, visit, heights[r][c])
@@ -22,29 +18,28 @@ class Solution:
             dfs(r, c + 1, visit, heights[r][c])
             dfs(r, c - 1, visit, heights[r][c])
 
-                
 
-        for c in range(cols):
-            dfs(0, c, pac, heights[0][c]) 
-            dfs(rows - 1, c, atl, heights[rows - 1][c])
 
-        for r in range(rows):
+        for r in range(rows): 
             dfs(r, 0, pac, heights[r][0])
             dfs(r, cols - 1, atl, heights[r][cols - 1])
+        
+        for c in range(cols):
+            dfs(0, c, pac, heights[0][c])
+            dfs(rows - 1, c, atl, heights[rows - 1][c])
+
 
         
         ans = [] 
 
-        for cordinate in atl:
-            if cordinate in pac:
+        for cordinate in pac:
+            if cordinate in atl:
                 ans.append(cordinate)
-        
-        print(ans)
 
         return ans
 
         
 
-
         
-
+        
+        
