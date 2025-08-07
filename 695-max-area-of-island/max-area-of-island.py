c0c1818563a2a6a0cdfbc5b870745eed7c9b8ddb
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-08-07 12:55:45 -0400

    Time: 38 ms (16.00%) | Memory: 18.2 MB (65.12%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,39 +1,32 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         
+
         rows = len(grid)
         cols = len(grid[0])
 
-        
         def dfs(r, c):
-            nonlocal count
-            if ( min(r, c) < 0
-                or r == rows or c == cols or grid[r][c] == 0):
+            nonlocal count 
 
+            if (min(r, c) < 0 or r == rows or c == cols 
+                or grid[r][c] == 0):
                 return 
-            
-            grid[r][c] = 0
-            count += 1
+
+            count += 1 
+            grid[r][c] = 0 
 
             dfs(r + 1, c)
-            dfs(r - 1, c) 
+            dfs(r - 1, c)
             dfs(r, c + 1)
             dfs(r, c - 1)
 
-            return
-
-
-        max_count = 0 
+        ans = 0 
 
         for r in range(rows):
-            for c in range(cols): 
-                if grid[r][c] == 1:
-                    count = 0 
-                    dfs(r, c)
-                    max_count = max(max_count, count) 
-
-        return max_count
+            for c in range(cols):
+                count = 0 
+                dfs(r, c)
+                ans = max(ans, count)
         
+        return ans
 
-
-                
