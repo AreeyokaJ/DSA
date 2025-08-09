a22b1136a1826d3559e9c6ca7cedab33ffdb8036
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-08-09 09:04:40 -0400

    Time: 3 ms (71.07%) | Memory: 18.5 MB (54.27%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -3,27 +3,30 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        rows = len(matrix)
-        cols = len(matrix[0])
-        dummy = copy.deepcopy(matrix)
+        
+        rows, cols = len(matrix), len(matrix[0])
+        rowZero = False
 
+        # determine which rows/cols need to be zero 
         for r in range(rows):
             for c in range(cols):
-                if dummy[r][c] == 0:
-                    self.zerofy(matrix, r, c)
-                
-
-    
-
-    def zerofy(self, matrix, r, c):
-        rows = len(matrix)
-        cols = len(matrix[0])
-
-        #keep row the same and start at zeroth columns 
-        for col in range(cols):
-            matrix[r][col] = 0
-
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0 
+                    
+                    if r > 0:
+                        matrix[r][0] = 0 
+                    else: 
+                        rowZero = True 
         
-        #kep col the same and start at the zeroth row 
-        for row in range(rows):
-            matrix[row][c] = 0
+        for r in range(1, rows):
+            for c in range(1, cols):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0 
+
+        if matrix[0][0] == 0:
+            for r in range(rows):
+                matrix[r][0] = 0
+
+        if rowZero:
+            for c in range(cols):
+                matrix[0][c] = 0
