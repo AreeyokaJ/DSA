55dbb726cb0d65ccdd1c6290144ec312cbabe953
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-08-09 07:49:40 -0400

    Time: 0 ms (100.00%) | Memory: 18.1 MB (19.57%) - LeetSync

--- 68-text-justification/text-justification.py
+++ 68-text-justification/text-justification.py
@@ -1,52 +1,36 @@
 class Solution:
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
-
-        res = [] 
-
-        line, length = [], 0 
-
-        i = 0 
-
-        while i < len(words):
-
-            if len(line) + len(words[i]) + length> maxWidth: 
-                #line complete 
-
-                #extra_space 
-                extra_space = maxWidth - length
-
-                #spaces  
-                spaces = extra_space // max(1, len(line) - 1) 
-
-                #remainder 
-                remainder = extra_space % max(1, len(line) - 1) 
-
-                for j in range(max(1, len(line) - 1)): 
-                    line[j] += " " * spaces 
-                  
-                    if remainder: 
-                        line[j] += " " 
-                        remainder -= 1 
-
-                res.append("".join(line))
         
-                #reset line 
-                line = [] 
-                length = 0 
-            
+        line,length = [], 0 
+
+        res = []
+
+        for i in range(len(words)):
+
+            if len(line) + len(words[i]) + length > maxWidth:
+
+                extra_space = maxWidth - length 
+                spaces = extra_space // max((len(line) - 1), 1)
+                remainder = extra_space % max(len(line) - 1, 1) 
+
+                for j in range(max(len(line) - 1, 1)): 
+                    line[j] += " " * spaces 
+                    if remainder:
+                        line[j] += " " 
+                        remainder -= 1
+                
+                res.append("".join(line))
+                line = []
+                length = 0
 
             line.append(words[i])
             length += len(words[i])
-            i += 1
-    
-        #handle last line 
-        #last_line = " ".join(line)
-        #trailing = maxWidth - len(last_line) 
-        #res.append(last_line + " "  * trailing)
 
-        extra_spaces = maxWidth - length - (len(line) - 1)  
-        res.append(" ".join(line) + " " * extra_spaces)
+        last_line = " ".join(line)
+        extra_space = maxWidth -len(last_line)
+        
+        res.append(last_line + " " * extra_space)
+
         return res
-
-
+        
 
