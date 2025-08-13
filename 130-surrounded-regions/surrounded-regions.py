2c42280e12eaae2cee8e6756bc5d284a7b409d0c
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-08-13 06:40:15 -0400

    Time: 3 ms (86.76%) | Memory: 21.7 MB (56.96%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -7,44 +7,39 @@ class Solution:
         rows = len(board)
         cols = len(board[0])
 
+        #dfs change all the boxes with O's to T's 
         def dfs(r, c):
-            if (min(r, c) < 0 or r == rows or c == cols 
-                or board[r][c] != 'O'):
-                return 
-            
-            board[r][c] = 'T'
+            if min(r, c) < 0 or r == rows or c == cols or board[r][c] != "O":
+                return
 
+            board[r][c] = 'T'
 
             dfs(r + 1, c)
             dfs(r - 1, c)
             dfs(r, c + 1)
-            dfs(r, c - 1 )
+            dfs(r, c - 1)
+            return
 
-        
-        #perform dfs alg on all the borders 
-        for r in range(rows): 
+        #run dfs on boarders 
+        for r in range(rows):
             dfs(r, 0)
             dfs(r, cols - 1)
 
-
         for c in range(cols):
             dfs(0, c)
-            dfs(rows - 1, c) 
+            dfs(rows - 1, c)
 
-        #mark all of the O's with X 
+        #change all O's to X's 
+
         for r in range(rows):
             for c in range(cols):
                 if board[r][c] == 'O':
                     board[r][c] = 'X'
 
-        #turn all the T's to O's 
-        for r in range(rows):
+        #change all T's to O's 
+
+        for r in range(rows): 
             for c in range(cols):
                 if board[r][c] == 'T':
                     board[r][c] = 'O'
-
-
         
-
-
-            
