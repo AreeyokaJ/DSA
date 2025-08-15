9964536f284f19615e83d6817d5bee3f878f25ad
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-08-15 06:59:11 -0400

    Time: 9 ms (18.45%) | Memory: 19.5 MB (31.24%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -4,27 +4,31 @@ class Solution:
         preMap = {i:[] for i in range(numCourses)}
 
         for course, preReq in prerequisites:
-            preMap[course].append(preReq) 
+            preMap[course].append(preReq)
 
         
         visit = set() 
 
         def dfs(course):
             if course in visit:
-                return False
-            if preMap[course] == []:
-                return True
+                return False 
 
+            if preMap[course] == []:
+                return True 
+            
             visit.add(course)
 
             for preReq in preMap[course]:
-                if not dfs(preReq): return False
+                if not dfs(preReq): return False 
             
             visit.remove(course)
             preMap[course] = []
             return True
+
         
         for course in range(numCourses):
-            if not dfs(course): return False
-
+            if not dfs(course): return False 
+        
         return True
+        
+
