23f6ab23687f370702b026176e1696ed4d554974
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-08-15 06:37:50 -0400

    Time: 0 ms (100.00%) | Memory: 18.1 MB (19.68%) - LeetSync

--- 68-text-justification/text-justification.py
+++ 68-text-justification/text-justification.py
@@ -1,36 +1,31 @@
 class Solution:
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
         
-        line,length = [], 0 
-
-        res = []
-
-        for i in range(len(words)):
-
-            if len(line) + len(words[i]) + length > maxWidth:
-
-                extra_space = maxWidth - length 
+        line, length = [], 0 
+        ans = []
+        for word in words:
+            if length + len(word) + len(line) > maxWidth: 
+                extra_space = maxWidth - (length)
                 spaces = extra_space // max((len(line) - 1), 1)
-                remainder = extra_space % max(len(line) - 1, 1) 
-
-                for j in range(max(len(line) - 1, 1)): 
-                    line[j] += " " * spaces 
+                     
+                remainder = extra_space % max((len(line) - 1), 1) 
+                for i in range(max(1, len(line) - 1)): 
+                    line[i] += " " * spaces 
+                    
                     if remainder:
-                        line[j] += " " 
-                        remainder -= 1
-                
-                res.append("".join(line))
-                line = []
+                        line[i] += " "
+                        remainder -= 1 
+
+                ans.append("".join(line))
+                line = [] 
                 length = 0
-
-            line.append(words[i])
-            length += len(words[i])
-
-        last_line = " ".join(line)
-        extra_space = maxWidth -len(last_line)
+            
+            line.append(word)
+            length += len(word) 
         
-        res.append(last_line + " " * extra_space)
-
-        return res
+        last_line = " ".join(line) 
+        extra_space = maxWidth - len(last_line)
+        ans.append(last_line + " " * extra_space)
         
+        return ans
 
