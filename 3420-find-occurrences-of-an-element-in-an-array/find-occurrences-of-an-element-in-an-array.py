9928f193d4013fbd60a8f7eba6927a22b50f0bb0
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-08-20 19:27:42 -0400

    Time: 51 ms (61.72%) | Memory: 33 MB (31.95%) - LeetSync

--- 3420-find-occurrences-of-an-element-in-an-array/find-occurrences-of-an-element-in-an-array.py
+++ 3420-find-occurrences-of-an-element-in-an-array/find-occurrences-of-an-element-in-an-array.py
@@ -1,19 +1,21 @@
 class Solution:
     def occurrencesOfElement(self, nums: List[int], queries: List[int], x: int) -> List[int]:
         
-    
-        locations = []
+        occurrences = {} 
+
+        occurrence = 1 
+
+        for i in range(len(nums)): 
+            if nums[i] == x: 
+                occurrences[occurrence] = i 
+                occurrence += 1 
+
         ans = []
-
-        for i, num in enumerate(nums):
-            if num == x:
-                locations.append(i)
-
-        
-        for query in queries:
-            if query > len(locations):
+        for query in queries: 
+            if query not in occurrences: 
                 ans.append(-1)
             else:
-                ans.append(locations[query-1])
+                ans.append(occurrences[query])
+
 
         return ans
