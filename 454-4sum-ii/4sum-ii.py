035c52149272bb04f7164e355d8615bf751da657
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-08-21 16:52:55 -0400

    Time: 356 ms (46.10%) | Memory: 18 MB (77.31%) - LeetSync

--- 454-4sum-ii/4sum-ii.py
+++ 454-4sum-ii/4sum-ii.py
@@ -1,16 +1,17 @@
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
         
-        sums = defaultdict(int) 
+        hashmap = defaultdict(int)
         ans = 0 
 
-        for a in nums1:
-            for b in nums2: 
-                sums[a+b] += 1 
+        for i in nums1: 
+            for j in nums2: 
+                hashmap[i + j] += 1
 
-        
-        for a in nums3: 
-            for b in nums4:
-                ans += sums[-1 *(a+b)]
+        for j in nums3:
+            for k in nums4: 
+                if -1 * (j + k) in hashmap: 
+                    ans += hashmap[-1 * (j +k)] 
 
         return ans
+                    
