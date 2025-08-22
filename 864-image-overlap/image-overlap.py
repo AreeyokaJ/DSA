646938149f63d1867cc7e34d17b5d910f36acd46
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-08-22 14:42:40 -0400

    Time: 1046 ms (20.18%) | Memory: 18 MB (82.78%) - LeetSync

--- 864-image-overlap/image-overlap.py
+++ 864-image-overlap/image-overlap.py
@@ -1,26 +1,24 @@
 class Solution:
     def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
         
-        N = len(img1)
-        ans = 0 
+        N = len(img1) 
 
-        def matches(x_shift, y_shift):
-            
-            count = 0 
 
-            for r in range(N): 
+        def shift(x_shift, y_shift): 
+            count = 0
+
+            for r in range(N):
                 for c in range(N): 
-                    if  0 <= c + x_shift < N and 0 <= r + y_shift < N and img1[r][c] == 1 and img2[r + y_shift][c + x_shift] == 1:
+                    if (0 <= r + y_shift < N and 0 <= c + x_shift < N and 
+                        img1[r][c] == 1 and img2[r + y_shift][c + x_shift] == 1):
                         count += 1 
-            
+
             return count 
 
+        ans = 0 
 
         for x in range(-N, N):
-            for y in range(-N, N):
-                ans = max(matches(x, y), ans)
+            for y in range(-N, N): 
+                ans = max(ans, shift(x, y))
 
         return ans
-
-        
-    
