a9bb54a7f03ca23d852239096b501eb6566fa6a9
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-08-26 18:38:30 -0400

    Time: 73 ms (46.30%) | Memory: 18.2 MB (39.78%) - LeetSync

--- 271-encode-and-decode-strings/encode-and-decode-strings.py
+++ 271-encode-and-decode-strings/encode-and-decode-strings.py
@@ -6,30 +6,27 @@ class Codec:
         res = ""
 
         for s in strs: 
-            res += str(len(s)) + "#" + s 
+            res += str(len(s)) + "#" + s
 
-        return res
+        return res 
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-        
-        i, ans = 0, []
-
-        while i < len(s):
-
+        res = []
+        i = 0 
+        while i < len(s): 
+            
             j = i 
-
             while s[j] != "#":
-                j += 1
-
+                j += 1 
+            
             length = int(s[i:j])
 
-            11#hello12#pizza
-            ans.append(s[j + 1: j + 1 + length])
-            i = j + 1 + length 
-
-        return ans
+            res.append(s[j + 1: j + length + 1])
+            i = j + length + 1 
+        
+        return res 
 
 
 
