2c52b87e32f0d6ff6f1d3566e6a9283230df93c1
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-08-28 14:03:03 -0400

    Time: 39 ms (40.73%) | Memory: 19.4 MB (65.17%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,37 +1,48 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         
-        pac = set()
-        atl = set()
-
         rows = len(heights)
         cols = len(heights[0])
 
-        ans = []
+        atl = set()
+        pac = set()
 
-        def dfs(r, c, visit, prev):
-            if (min(r, c) < 0 or r == rows or c == cols or
-                 (r,c) in visit or heights[r][c] < prev):
-                return 
+        def dfs(r, c, last, seen):
 
-            visit.add((r,c))
-            
-            dfs(r + 1, c, visit, heights[r][c])
-            dfs(r - 1, c, visit, heights[r][c])
-            dfs(r, c + 1, visit, heights[r][c])
-            dfs(r, c - 1, visit, heights[r][c])
+            if (min(r, c) < 0 or r == rows or
+                c == cols or (r, c) in seen or heights[r][c] < last):
+                return
 
+
+            seen.add((r, c))
+
+            dfs(r + 1, c, heights[r][c], seen)
+            dfs(r - 1, c, heights[r][c], seen)
+            dfs(r, c + 1, heights[r][c], seen)
+            dfs(r, c - 1, heights[r][c], seen)
+
+            return 
+        
+        #perform dfs among the borders 
+        
+        for c in range(cols): 
+            dfs(0, c, heights[0][c], pac)
+            dfs(rows - 1, c, heights[rows - 1][c], atl)
 
         for r in range(rows):
-           dfs(r, 0, pac, heights[r][0])
-           dfs(r, cols - 1 , atl, heights[r][cols-1])
+            dfs(r, 0, heights[r][0], pac)
+            dfs(r, cols - 1, heights[r][cols -1], atl)
 
-        for c in range(cols):
-            dfs(0, c, pac, heights[0][c])
-            dfs(rows - 1, c, atl, heights[rows-1][c])
+        
+        #return the ones that are in both sets 
+        ans = [] 
 
-        for cordinate in pac:
-            if cordinate in atl:
-                ans.append(cordinate)
+        for coordinate in atl:
+            if coordinate in pac:
+                ans.append(coordinate)
 
         return ans
+
+
+
+
