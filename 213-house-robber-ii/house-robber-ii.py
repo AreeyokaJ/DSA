c1be393428439c910a45f8a5e4c1205187734a1b
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-09-02 17:32:49 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (17.56%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,16 +1,17 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        return max(nums[0], self.money(nums[1:]), self.money(nums[0:len(nums) - 1]))
+        return max(nums[0], self.money(nums[1:len(nums)]), self.money(nums[0:len(nums)-1]) )
 
     def money(self, nums):
 
         rob1 = 0 
         rob2 = 0 
 
-        for n in nums: 
-            temp = max(rob1 + n, rob2)
+        for num in nums:
+
+            temp = max(rob1 + num, rob2)
             rob1 = rob2 
-            rob2 = temp
+            rob2 = temp 
 
         return rob2
