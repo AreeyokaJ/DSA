daec6af0cb2f410a58a333b942fa02a1dd73ec38
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-09-05 05:46:55 -0400

    Time: 3 ms (47.57%) | Memory: 20.1 MB (7.36%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.py
+++ 261-graph-valid-tree/graph-valid-tree.py
@@ -1,31 +1,40 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
         
-        adjacency = {i:[] for i in range(n)}
+        adj = {i:[] for i in range(n)}
 
 
         for n1, n2 in edges:
-            adjacency[n1].append(n2)
-            adjacency[n2].append(n1)
+            adj[n1].append(n2)
+            adj[n2].append(n1)
+
 
         
         visit = set()
 
-
         def dfs(node, prev):
+            
             if node in visit:
                 return False 
 
+            
             visit.add(node)
 
-            for nei in adjacency[node]:
+            for nei in adj[node]:
                 if prev == nei:
-                    continue 
-                
-                if not dfs(nei, node): return False 
+                    continue
+
+                if not dfs(nei, node): 
+                    return False
             
-
-            return True 
-
+            return True
         
-        return dfs(0, -1) and len(visit) == n
+
+        if not dfs(0, -1):
+            return False
+        
+        if len(visit) == n:
+            return True
+        else:
+            return False
+
