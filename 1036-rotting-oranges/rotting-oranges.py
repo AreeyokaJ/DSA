2279b816f5a2e89ea7ec2f6281276e13b6aa1361
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-09-07 15:39:00 -0400

    Time: 3 ms (81.46%) | Memory: 17.9 MB (44.84%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,47 +1,41 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-
-
-        #only fresh fruit 
-        #only have rotten fruit  : rotten fruit > 0 fresh fruit == 0  0 minutes to acheive our goal
         
 
-        rows = len(grid)
-        cols = len(grid[0]) 
-        rotten = 0 
-        fresh = 0 
+        rows = len(grid) 
+        cols = len(grid[0])
+        neighbors = [[0, 1], [1,0], [-1, 0], [0, -1]]
+        fruit = 0
         queue = deque()
-        neighbors = [[0, 1], [1, 0], [-1, 0], [0, -1]]
-
-
-
+        
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 2:
-                    queue.append((r,c))
-
+                    queue.append((r, c))
+                
                 if grid[r][c] == 1:
-                    fresh += 1 
-
+                    fruit += 1 
         
         minutes = 0 
 
-        while queue and fresh > 0:
-        
-          
+        while queue and fruit > 0: 
+
             for i in range(len(queue)):
-                r, c = queue.popleft()
+
+                r, c = queue.popleft() 
 
                 for dr, dc in neighbors:
-                    if (min(r + dr, c + dc) < 0 or dr + r == rows or 
-                        dc + c == cols or grid[dr + r][dc + c] != 1):
+                    nr = dr + r 
+                    nc = dc + c 
+                    if (min(nr, nc) < 0 or nr == rows or nc == cols
+                        or grid[nr][nc] != 1):
                         continue 
+                    
+                    grid[nr][nc] = 2 
+                    queue.append((nr, nc))
+                    fruit -= 1
 
-                    queue.append((dr + r, dc + c))
-                    grid[dr + r][dc + c] = 2
-                    fresh -= 1 
-            
+             
             minutes += 1 
-
         
-        return minutes if fresh == 0 else  -1 
+        return minutes if (fruit) == 0 else -1
