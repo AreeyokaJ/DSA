afddd4a7a9e01ca09a23ccb6b87f6577390d823a
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-09-16 11:25:23 -0400

    Time: 0 ms (100.00%) | Memory: 18.1 MB (38.52%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,24 +1,31 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         
-        subset = [] 
-        ans = [] 
+        res = [] 
+        subset = []
+        array = nums
 
-        nums.sort()
+        array.sort()
+
+        #[1, 2, 2,  3]  2^n decisions
+
+        #Time complexity O(2^n * n )
+        #Space complexity O(n) 
+
         def dfs(i):
-            if i == len(nums):
-                ans.append(subset.copy())
-                return
-
+            if i == len(array): 
+                res.append(subset.copy())
+                return  
+            
             subset.append(nums[i])
-            dfs(i + 1) 
+            dfs(i + 1)
 
             subset.pop()
 
-            while i + 1 < len(nums) and nums[i + 1] == nums[i]:
+            while i + 1 < len(array) and nums[i] == nums[i + 1]:
                 i += 1 
 
-            dfs(i + 1)
+            dfs(i + 1) 
         
         dfs(0)
-        return ans
+        return res
