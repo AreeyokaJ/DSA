d9a011394eeeb479966c3d15ac88877c88c3d394
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-09-18 14:15:01 -0400

    Time: 4 ms (72.83%) | Memory: 21.6 MB (88.61%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -7,39 +7,43 @@ class Solution:
         rows = len(board)
         cols = len(board[0])
 
-        #dfs change all the boxes with O's to T's 
-        def dfs(r, c):
-            if min(r, c) < 0 or r == rows or c == cols or board[r][c] != "O":
-                return
+#perform dfs on the boarders, essensitally if we find O's connected to the borders then they are not to be captured, replace those O's with T's so that we know which O's are the ones that need to be changed.  
 
-            board[r][c] = 'T'
+        def dfs(r, c): 
+            if min(r, c) < 0 or r == rows or c == cols:
+                return 
+            
+            if board[r][c] != "O": 
+                return 
+            
+            board[r][c] = "T"
 
             dfs(r + 1, c)
             dfs(r - 1, c)
-            dfs(r, c + 1)
-            dfs(r, c - 1)
-            return
+            dfs(r, c+ 1)
+            dfs(r, c- 1) 
 
-        #run dfs on boarders 
+            return 
+
+    
         for r in range(rows):
             dfs(r, 0)
             dfs(r, cols - 1)
 
         for c in range(cols):
             dfs(0, c)
-            dfs(rows - 1, c)
-
-        #change all O's to X's 
+            dfs(rows - 1, c) 
+        
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "O":
+                    board[r][c] =  "X"
 
         for r in range(rows):
             for c in range(cols):
-                if board[r][c] == 'O':
-                    board[r][c] = 'X'
-
-        #change all T's to O's 
-
-        for r in range(rows): 
-            for c in range(cols):
-                if board[r][c] == 'T':
-                    board[r][c] = 'O'
+                if board[r][c] == "T":
+                    board[r][c] = "O"
         
+
+
+
