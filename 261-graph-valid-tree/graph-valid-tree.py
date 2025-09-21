b16c52220e8ebf60c6180a2c09e078b07794019d
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-09-21 12:50:40 -0400

    Time: 5 ms (18.13%) | Memory: 20.2 MB (7.00%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.py
+++ 261-graph-valid-tree/graph-valid-tree.py
@@ -1,41 +1,32 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        
-        #graph must be connected 
-        #graph has no cycles 
+    
+        #tree is connected 
+        #no cycles 
 
-        #cycle detection alg
-
-        #as we perform dfs make sure that we visited all of the nodes 
-
-        adj = {i:[] for i in range(n)} 
+        adj = {i:[] for i in range(n)}
 
         for n1, n2 in edges:
-            adj[n1].append(n2) 
+            adj[n1].append(n2)
             adj[n2].append(n1) 
 
-        
+
         visit = set() 
 
         def dfs(node, prev):
             if node in visit:
-                return False 
+                return False
 
-            visit.add(node) 
-
+            visit.add(node)
             for nei in adj[node]:
-                if nei == prev: 
+                if nei == prev:
                     continue 
-
-                if not dfs(nei, node): return False 
+                
+                if not dfs(nei, node): return False
 
             return True 
-        
-        
-        
-        
+
         
         return True if dfs(0, -1) and len(visit) == n else False
+            
 
-            
-            
