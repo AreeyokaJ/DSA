43c3c446404a1d3c223449b7617885739ac4d64f
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-09-22 16:12:38 -0400

    Time: 48 ms (47.45%) | Memory: 21.4 MB (53.93%) - LeetSync

--- 286-walls-and-gates/walls-and-gates.py
+++ 286-walls-and-gates/walls-and-gates.py
@@ -3,42 +3,40 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        
-        rows = len(rooms)
-        cols = len(rooms[0])
-        queue = deque() 
-
-        def add_rooms(r, c):
-            nonlocal d 
+         
+        def addRooms(r, c):
+            nonlocal d
 
             if min(r, c) < 0 or r == rows or c == cols:
                 return 
-            
+
             if rooms[r][c] != 2147483647:
                 return 
-
-            rooms[r][c] = d 
+            
             queue.append((r, c))
+            rooms[r][c] = d
 
+
+        
+        rows = len(rooms)
+        cols = len(rooms[0]) 
+        queue = deque()
 
         for r in range(rows):
             for c in range(cols):
                 if rooms[r][c] == 0:
                     queue.append((r, c))
-                
-        
-        d = 1
+
+        d = 1 
 
         while queue:
 
             for i in range(len(queue)):
                 r, c = queue.popleft()
 
-                add_rooms(r + 1, c)
-                add_rooms(r - 1, c)
-                add_rooms(r, c + 1)
-                add_rooms(r, c - 1)
-            
-            d += 1 
+                addRooms(r + 1, c)
+                addRooms(r - 1, c)
+                addRooms(r, c + 1)
+                addRooms(r, c - 1) 
 
-    
+            d += 1 
