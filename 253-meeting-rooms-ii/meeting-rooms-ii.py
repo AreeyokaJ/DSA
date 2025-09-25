3745b7cf69552bfcc6cc8f7dc0b4f8fce99b1daa
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-09-25 12:56:48 -0400

    Time: 4 ms (54.96%) | Memory: 20.1 MB (36.60%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,22 +1,20 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         
-        start = sorted(i[0] for i in intervals)
-        end = sorted(i[1] for i in intervals) 
-
+        start = sorted([i[0] for i in intervals] )
+        end = sorted([i[1] for i in intervals] )
         s = 0 
         e = 0 
-
-        count = 0 
-        ans = 0 
+        count = 0
+        max_count = 0
         while s < len(start): 
-            if start[s] < end[e]: 
+            if start[s] < end[e]:
+                s += 1 
                 count += 1 
-                ans = max(count, ans) 
-                s += 1
-
-            else: 
-                count -= 1 
-                e += 1 
-        
-        return ans
+                max_count = max(count, max_count)
+            
+            else:
+                e += 1
+                count-= 1  
+        return max_count
+            
