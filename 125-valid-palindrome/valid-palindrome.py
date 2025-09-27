6b2e07ff7b9a4cbdcec85df595fbfcef15c3f13f
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-09-27 17:11:52 -0400

    Time: 7 ms (81.84%) | Memory: 23.3 MB (10.63%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -3,16 +3,16 @@ class Solution:
         
         s = "".join(c.lower() for c in s if c.isalnum())
 
-        l = 0
+
+        l = 0 
         r = len(s) - 1 
 
-
         while l < r:
+            
             if s[l] != s[r]:
-                return False
-
+                return False 
+            
             l += 1 
             r -= 1 
-
 
         return True
