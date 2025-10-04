1d8ef7772898fa9fa701d5c8fcb1fbc11a77841f
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-10-04 19:41:15 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (26.18%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -3,43 +3,39 @@ class Solution:
         
         neighbors = [[0, 1], [1, 0], [-1, 0], [0, -1]] 
 
+        fresh_fruit = 0 
+
+        queue = deque()
+
         rows = len(grid)
         cols = len(grid[0]) 
-        queue = deque() 
-        fresh_fruit = 0 
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 2:
                     queue.append((r,c))
-
                 elif grid[r][c] == 1:
                     fresh_fruit += 1
 
-            
-        minutes = 0 
+        
+        minutes = 0
 
-        while queue and fresh_fruit > 0:
+        while queue and fresh_fruit > 0: 
             for i in range(len(queue)):
                 r, c = queue.popleft() 
 
-                for dr, dc in neighbors:
-                    nr = r + dr 
-                    nc = c + dc
+                for dr, dc in neighbors: 
+                    nr = dr + r 
+                    nc = dc + c 
 
-                    if nr == rows or nc == cols or min(nr, nc) < 0:
+                    if (min(nr, nc) < 0 or nr == rows or nc == cols
+                        or grid[nr][nc] != 1):
                         continue 
-                    
-                    if grid[nr][nc] != 1:
-                        continue  
 
                     grid[nr][nc] = 2 
+                    queue.append((nr, nc))
                     fresh_fruit -= 1 
-                    queue.append((nr,nc))
+        
+            minutes += 1
 
-            minutes += 1 
-
-
-        return minutes if fresh_fruit == 0 else -1 
-
-  
+        return minutes if fresh_fruit == 0 else  -1
