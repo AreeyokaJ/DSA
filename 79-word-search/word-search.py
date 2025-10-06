38361188a1b7a0119f363349adc957169b252568
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-10-06 14:56:13 -0400

    Time: 3048 ms (87.08%) | Memory: 17.5 MB (99.37%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,32 +1,27 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        rows, cols = len(board), len(board[0]) 
-        visit = set() 
+        rows, cols = len(board), len(board[0])
 
         def dfs(r, c, i):
             if i == len(word):
                 return True
-
-            if min(r, c) < 0 or r == rows or c == cols:
-                return False 
-
-            if board[r][c] != word[i] or (r, c) in visit:
-                return False 
-             
-            visit.add((r,c)) 
+            if r < 0 or c < 0 or r >= rows or c >= cols or board[r][c] != word[i]:
+                return False
             
-            res = (dfs(r + 1, c, i + 1) or 
-            dfs(r - 1, c, i + 1) or
-            dfs(r, c + 1, i + 1) or
-            dfs(r, c - 1, i + 1) )
-
-            visit.remove((r, c))
-
+            temp = board[r][c]
+            board[r][c] = "#"  # mark as visited
+            
+            res = (dfs(r + 1, c, i + 1) or
+                   dfs(r - 1, c, i + 1) or
+                   dfs(r, c + 1, i + 1) or
+                   dfs(r, c - 1, i + 1))
+            
+            board[r][c] = temp  # unmark
             return res
 
         for r in range(rows):
             for c in range(cols):
                 if dfs(r, c, 0):
-                    return True  
-
+                    return True
+        return False
         return False
