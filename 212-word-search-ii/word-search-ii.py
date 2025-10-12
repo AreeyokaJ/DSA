43523ca6cbfce583b2963e6983e7700fc3ae7396
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-10-12 12:36:28 -0400

    Time: 864 ms (76.73%) | Memory: 20.9 MB (5.64%) - LeetSync

--- 212-word-search-ii/word-search-ii.py
+++ 212-word-search-ii/word-search-ii.py
@@ -1,83 +1,69 @@
 class TrieNode:
     def __init__(self):
         self.children = {}
-        self.word = False 
+        self.word = False
     
-    def add_word(self, word):
-        curr = self
-        for c in word: 
-            if c not in curr.children:
-                curr.children[c] = TrieNode()
-
-            curr = curr.children[c]
-        
-        curr.word = True 
-    
-    def removeWord(self, word) -> None:
+    def addWord(self, word):
         curr = self 
-        storage = [] 
 
         for c in word:
-            storage.append([curr, c])
-            curr = curr.children[c]
-        
-        for parentNode, childkey in reversed(storage):
-            childNode = parentNode.children[childkey]
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
             
-            if len(childNode.children) == 0: 
-                del parentNode.children[childkey]
+            curr = curr.children[c] 
+        
+        curr.word = True
+    
+    def removeWord(self, word):
+        parentAndChildKey = []
+        curr = self 
+        for c in word:
+            parentAndChildKey.append([curr, c])
+            curr = curr.children[c] 
 
-
+        for parent, childKey in reversed(parentAndChildKey):
+            if not parent.children[childKey].children:
+                del parent.children[childKey]
 
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         
-        root = TrieNode()
-        for word in words:
-            root.add_word(word) 
+        #add words to Trie 
+        root = TrieNode() 
         
+        for word in words:
+            root.addWord(word)
 
-
-        ans = set()
+        rows = len(board)
+        cols = len(board[0]) 
 
         visit = set()
-        rows = len(board)
-        cols = len(board[0])
+        res = set()
 
-
-        def dfs(r, c, word, node):
-
-            if (min(r, c) < 0 or r == rows or
-                c == cols or (r, c) in visit):
+        def dfs(r, c, node, prefix):
+            if (min(r, c) < 0 or r == rows or c == cols or 
+                 board[r][c] not in node.children or (r, c) in visit):
                 return 
             
-            if board[r][c] not in node.children:
-                return 
             visit.add((r, c))
-           
+            char = board[r][c]
 
-            word += board[r][c]
-            node = node.children[board[r][c]]
-            if node.word:
-                ans.add(word)
-                root.removeWord(word)
-                
+            node = node.children[char]
+            prefix += char
+            if node.word == True:
+                res.add(prefix)
+                root.removeWord(prefix) 
 
-            dfs(r + 1, c, word, node)
-            dfs(r - 1, c, word, node)
-            dfs(r, c + 1, word, node)
-            dfs(r, c - 1, word, node) 
-
+            dfs(r + 1, c, node, prefix)
+            dfs(r - 1, c, node, prefix)
+            dfs(r, c + 1, node, prefix)
+            dfs(r, c - 1, node, prefix)
             visit.remove((r,c))
+            return 
 
-        
         for r in range(rows):
             for c in range(cols):
-                dfs(r, c, "", root)
-
-        return list(ans)
+                dfs(r, c, root, "")
 
 
-
-
-            
+        return list(res)
