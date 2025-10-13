82c57abe46e592687218cb0ee75930f78b61d783
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-10-13 11:13:19 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (30.21%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,20 +1,16 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        
         if n == 1:
             return 1 
-        
         if n == 2:
             return 2 
 
-        
-        i = 3 
+        i = 3
         dp = [1, 2]
-        while i <= n: 
+        while i <= n:
             temp = dp[1]
             dp[1] = dp[0] + dp[1]
-            dp[0] = temp 
-            i += 1 
+            dp[0] = temp
+            i += 1
         
         return dp[1]
-            
