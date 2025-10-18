a54120be74195bd20a99a0d9ccd3b0e9bfe015bd
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-10-17 21:27:17 -0400

    Time: 4921 ms (28.08%) | Memory: 17.7 MB (95.95%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,36 +1,29 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        rows, cols = len(board), len(board[0]) 
+        rows = len(board)
+        cols = len(board[0])
 
         def dfs(r, c, i):
             if i == len(word):
-                return True
+                return True 
 
-            #out of bounds 
-            if (min(r, c) < 0 or r == rows or c == cols or 
-             word[i] != board[r][c]):
-                return False
-            
+            if min(r,c) < 0 or r == rows or c == cols or board[r][c] != word[i] or board[r][c] == '#':
+                return False 
+
             temp = board[r][c]
-            board[r][c] = "#"
+            board[r][c] = '#'
+
             res = (
-                dfs(r + 1, c, i + 1) or
-                dfs(r - 1, c, i + 1) or
-                dfs(r, c + 1 , i + 1) or
-                dfs(r, c - 1, i + 1) 
+                dfs(r + 1, c, i + 1)
+                or dfs(r - 1, c, i + 1)
+                or dfs(r, c+ 1, i + 1) 
+                or dfs(r, c - 1, i + 1) 
             )
-            board[r][c] = temp
-        
-            return res
+            board[r][c] = temp 
+            return res 
 
-            
-
-
-
-        
         for r in range(rows):
             for c in range(cols):
                 if dfs(r, c, 0): return True 
-    
-
+        
         return False
