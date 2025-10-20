dbe814b77c324aedd5bf1b2463b49dabf137c2a1
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-10-20 13:57:18 -0400

    Time: 7 ms (95.83%) | Memory: 17.9 MB (90.96%) - LeetSync

--- 616-add-bold-tag-in-string/add-bold-tag-in-string.py
+++ 616-add-bold-tag-in-string/add-bold-tag-in-string.py
@@ -1,10 +1,10 @@
 class Solution:
     def addBoldTag(self, s: str, words: List[str]) -> str:
-        bold = [False] * len(s)
+        bold = [False] * len(s) 
 
 
         for word in words:
-            start = s.find(word) 
+            start = s.find(word)
             length = len(word) 
 
             while start != -1:
@@ -18,18 +18,17 @@ class Solution:
 
         i = 0 
 
-        while i < len(s): 
-            if bold[i]: 
+        while i < len(s):
+            if bold[i]:
                 ans.append("<b>")
 
                 while i < len(s) and bold[i]:
-                    ans.append(s[i])
+                    ans.append(s[i]) 
                     i += 1 
 
-                ans.append("</b>") 
-            else: 
+                ans.append("</b>")
+            else:
                 ans.append(s[i])
                 i += 1
-
 
         return "".join(ans)
