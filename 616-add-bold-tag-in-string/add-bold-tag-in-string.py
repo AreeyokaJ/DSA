5b415758af77ef17db550ed6ba88e58b213d59bc
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-10-20 15:07:34 -0400

    Time: 159 ms (15.83%) | Memory: 26.8 MB (5.74%) - LeetSync

--- 616-add-bold-tag-in-string/add-bold-tag-in-string.py
+++ 616-add-bold-tag-in-string/add-bold-tag-in-string.py
@@ -1,17 +1,54 @@
+class TrieNode: 
+    def __init__(self):
+        self.children = {}
+        self.word = False
+
+
 class Solution:
     def addBoldTag(self, s: str, words: List[str]) -> str:
         bold = [False] * len(s) 
 
+        #build Trie
+        root = TrieNode() 
 
-        for word in words:
-            start = s.find(word)
-            length = len(word) 
+        for word in words: 
+            curr = root 
 
-            while start != -1:
-                for i in range(start, start + length):
-                    bold[i] = True 
+            for c in word: 
+                if c not in curr.children:
+                    curr.children[c] = TrieNode()
+                
+                curr = curr.children[c] 
+            
+            curr.word =True
 
-                start = s.find(word, start + 1) 
+        
+        for i in range(len(s)):
+            j = i 
+
+            longest = -1 
+            curr = root
+            while j < len(s) and s[j] in curr.children:
+                curr = curr.children[s[j]]
+
+                if curr.word:
+                    longest = j 
+                
+                j += 1 
+
+            if longest != -1:
+                for k in range(i, longest + 1):
+                    bold[k] = True 
+
+        # for word in words:
+        #     start = s.find(word)
+        #     length = len(word) 
+
+        #     while start != -1:
+        #         for i in range(start, start + length):
+        #             bold[i] = True 
+
+        #         start = s.find(word, start + 1) 
 
         
         ans = [] 
