b08e0e6f0d36208855747c927cf9b7798c06c5a3
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-11-26 09:40:14 -0500

    Time: 43 ms (55.44%) | Memory: 21.3 MB (66.37%) - LeetSync

--- 286-walls-and-gates/walls-and-gates.py
+++ 286-walls-and-gates/walls-and-gates.py
@@ -3,36 +3,48 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        queue = deque()
-        rows = len(rooms)
-        cols = len(rooms[0])
-
-        def addRooms(r, c):
+        
+        #if the room != inf do nothing
+        def add_rooms(r, c):
+            
             if min(r, c) < 0 or r == rows or c == cols:
-                return
-            if rooms[r][c] != 2147483647:
                 return 
             
-            rooms[r][c] = d
+            if rooms[r][c] != 2147483647:
+                return 
+
             queue.append((r, c))
+            rooms[r][c] = d 
+
+
+        
+        queue = deque()
+        
+        rows = len(rooms)
+        cols = len(rooms[0]) 
 
         for r in range(rows):
             for c in range(cols):
                 if rooms[r][c] == 0:
-                    queue.append((r, c))
+                    queue.append((r, c)) 
+        
 
-        d = 1 
+        #keep track of the distance
+        d = 1
 
+        #perform bfs 
         while queue:
 
             for i in range(len(queue)):
                 r, c = queue.popleft()
-                
-                addRooms(r + 1, c)
-                addRooms(r - 1, c) 
-                addRooms(r, c + 1) 
-                addRooms(r, c - 1) 
-            
+                add_rooms(r + 1, c)
+                add_rooms(r - 1, c)
+                add_rooms(r, c + 1) 
+                add_rooms(r, c - 1)
+
+    
             d += 1 
 
-        
+
+
+        #update distance after each round
