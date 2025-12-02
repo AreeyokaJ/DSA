829f3da5752a6979a186ed4c8324ea85aeb4ceb3
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-12-02 06:45:57 -0500

    Time: 4 ms (35.49%) | Memory: 23.3 MB (37.94%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -16,32 +16,32 @@ class Solution:
         while fast and fast.next:
             slow = slow.next 
             fast = fast.next.next 
-        
-        #create two seperate lists 
-        first = head 
-        second = slow.next
+
+        #break the lists into two havles 
+        second = slow.next 
         slow.next = None
+        first = head
 
+        #reverse the second list 
 
-        #reverse the 2nd list
         prev = None 
 
-        while second:
-            temp = second.next 
-            second.next = prev 
-            prev = second 
+        while second: 
+            temp = second.next
+            second.next = prev
+            prev = second
             second = temp 
 
         second = prev
 
         #interleave the two lists 
-        
-        while second:
+    
+        while second: 
             temp1, temp2 = first.next, second.next 
 
             first.next = second
             second.next = temp1
 
-            first = temp1
-            second = temp2 
-        
+            first = temp1 
+            second = temp2
+    
