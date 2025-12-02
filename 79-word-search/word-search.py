664e920b9fa3f6752a944a5ac246b859774baf5f
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-12-02 06:29:18 -0500

    Time: 4867 ms (29.59%) | Memory: 17.8 MB (87.40%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -4,30 +4,35 @@ class Solution:
         rows = len(board)
         cols = len(board[0]) 
 
+
         def dfs(r, c, i):
             if i == len(word):
                 return True 
 
-            if (min(r, c) < 0 or r == rows or c == cols
-                or board[r][c] != word[i]):
-                return False
+            if (
+                min(r,c) < 0 or r == rows or c == cols or 
+                board[r][c] != word[i] 
+            ):
+                return False 
 
-            temp = board[r][c] 
-            board[r][c] = "#"
-
+            
+            temp = board[r][c]
+            board[r][c] = '#' 
 
             res = (
                 dfs(r + 1, c, i + 1) or
-                dfs(r - 1, c, i + 1) or 
-                dfs(r, c + 1, i + 1) or
-                dfs(r, c - 1, i + 1)
+                dfs(r - 1, c, i + 1) or
+                dfs(r, c + 1, i + 1) or 
+                dfs(r, c - 1 , i + 1) 
             )
 
             board[r][c] = temp
             return res
-        
-        for r in range(rows):
-            for c in range(cols):
+
+
+        for r in range(rows): 
+            for c in range(cols): 
                 if dfs(r, c, 0): return True
 
+        
         return False
