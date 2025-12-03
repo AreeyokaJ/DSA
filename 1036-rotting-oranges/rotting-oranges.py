e63a56f6e5b911894dda81f2112b7f1df52ab30d
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-12-03 06:21:35 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (70.76%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,43 +1,42 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        queue = deque() 
-
+        
         rows = len(grid)
         cols = len(grid[0]) 
 
+        queue = deque() 
+        neighbors = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
         fresh_fruit = 0 
-        neighbors = [[0, 1], [1, 0], [-1, 0], [0, -1]]
 
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 2:
-                    queue.append((r, c))
+                    queue.append((r,c))
                 
                 if grid[r][c] == 1:
                     fresh_fruit += 1 
-
-                
+        
         minutes = 0 
 
-        while queue and fresh_fruit > 0:
+        while queue and fresh_fruit:
+
             for i in range(len(queue)):
-                r, c = queue.popleft() 
+                r, c = queue.popleft()
 
-                for dr, dc in neighbors: 
-                    nr = r + dr 
-                    nc = c + dc 
+                for dr, dc in neighbors:
+                    nr = dr + r
+                    nc = dc + c 
 
-                    if (min(nr, nc) < 0 or nr == rows or nc == cols or 
-                        grid[nr][nc] != 1):
-                        continue
-
+                    if (min(nr, nc) < 0 or nr == rows or nc == cols or
+                     grid[nr][nc] != 1):
+                        continue 
+                    
+                    grid[nr][nc] = 2
                     queue.append((nr, nc))
                     fresh_fruit -= 1 
-                    grid[nr][nc] = 2
-            
             minutes += 1 
         
-        return minutes if fresh_fruit == 0 else -1
-
+        return minutes if not fresh_fruit else -1
 
