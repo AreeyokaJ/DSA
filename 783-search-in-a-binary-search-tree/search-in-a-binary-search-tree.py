db10b88ac5a5f835dca645aff9636de48ab007b0
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-12-06 16:32:09 -0500

    Time: 0 ms (100.00%) | Memory: 19.2 MB (67.64%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.py
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.py
@@ -6,12 +6,15 @@
 #         self.right = right
 class Solution:
     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        
         if not root:
-            return None
+            return None 
 
-        if root.val > val:
-            return self.searchBST(root.left, val)
         if root.val < val:
-            return self.searchBST(root.right, val)
-        if root.val == val:
+            return self.searchBST(root.right, val) 
+
+        elif root.val > val:
+            return self.searchBST(root.left, val) 
+
+        else:
             return root
