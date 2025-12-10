79375d2598096b29e2e19ea4d033a3f8d6393d88
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2025-12-10 14:02:11 -0500

    Time: 12 ms (22.05%) | Memory: 18.2 MB (95.94%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -5,17 +5,15 @@ class Solution:
         current_max = nums[0]
         global_max = nums[0]
 
-
-
         for num in nums[1:]:
+
             if num < 0:
                 temp = current_min 
-                current_min = current_max 
+                current_min = current_max
                 current_max = temp 
-
             
-            current_min = min(num * current_min, num) 
-            current_max = max(num * current_max, num)
-            global_max = max(global_max, current_max)
+            current_min = min(num,  num * current_min)
+            current_max = max(num, num * current_max)
+            global_max = max(current_max, global_max)
         
         return global_max
