c1ddb5b78a00a8fb9ceac9c92d94850f1de4c8bc
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2026-01-02 19:00:30 -0500

    Time: 132 ms (81.14%) | Memory: 31.7 MB (78.04%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -8,16 +8,17 @@ class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         
         def dfs(root, maxVal):
+
             if not root:
                 return 0 
+
+            maxVal = max(maxVal, root.val)
+            curr = 1 if root.val >= maxVal else 0 
             
-            res = 1 if root.val >= maxVal else 0 
-            maxVal = max(root.val, maxVal) 
+            res = curr + dfs(root.left, maxVal) + dfs(root.right, maxVal)
 
 
+            return res 
 
 
-            return res + dfs(root.left, maxVal) + dfs(root.right, maxVal) 
-
-        
         return dfs(root, root.val)
