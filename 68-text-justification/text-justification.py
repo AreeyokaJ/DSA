c5203d839a753c6ee3da655e48fc0c79edf779e0
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2026-01-13 18:36:02 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (9.20%) - LeetSync

--- 68-text-justification/text-justification.py
+++ 68-text-justification/text-justification.py
@@ -5,31 +5,32 @@ class Solution:
         length = 0 
         ans = []
 
-
         for word in words:
-            if len(word) + length + len(line) > maxWidth:
-                extra_spaces = maxWidth - length 
+
+            if len(line) + len(word) + length > maxWidth: 
+                extra_spaces = maxWidth - length
                 spaces = extra_spaces // max(1, len(line) - 1)
-                remainder = extra_spaces % max(1, len(line) - 1)
+                remainder = extra_spaces % max(1, len(line) - 1) 
 
                 for i in range(max(1, len(line) - 1)):
-                    line[i] += " " * spaces
+                    line[i] = line[i] + " " * spaces
 
-                    if remainder:
+                    if remainder: 
+                        line[i] += " "
                         remainder -= 1
-                        line[i] += " " 
                 
-                ans.append("".join(line))
-                length = 0 
-                line = []
+
+                ans.append("".join(line))    
+                length = 0
+                line = [] 
+
             
             line.append(word)
-            length += len(word)
+            length += len(word) 
 
         last_line = " ".join(line) 
+        extra_spaces = maxWidth - len(last_line) 
 
-        extra_spaces = maxWidth - len(last_line)
+        ans.append(last_line + " " * extra_spaces)
 
-        last_line += (" " * extra_spaces)
-        ans.append(last_line)
         return ans
