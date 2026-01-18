6cebeb75ab488cfc39c20e0470c219123da7e004
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2026-01-18 09:31:02 -0500

    Time: 6796 ms (18.66%) | Memory: 21.9 MB (5.28%) - LeetSync

--- 212-word-search-ii/word-search-ii.py
+++ 212-word-search-ii/word-search-ii.py
@@ -1,82 +1,64 @@
-class TrieNode: 
-    
+class TrieNode():
     def __init__(self):
         self.children = {} 
-        self.word = False
+        self.word = False 
 
-    def addWord(self, word):
+    
+    def add_word(self, word):
         curr = self 
-        
+
         for c in word:
             if c not in curr.children:
-                curr.children[c] = TrieNode()
-
-            curr = curr.children[c]
+                curr.children[c] = TrieNode() 
+            
+            curr = curr.children[c] 
         
         curr.word = True
-
-    def pruneWord(self, word):
-        
-        parentChild = [] 
-
-        curr = self
-
-        for c in word: 
-            parentChild.append([curr, c])
-            curr = curr.children[c]
-            
-            
-        
-        for parentNode, child in reversed(parentChild):
-            
-            childNode = parentNode.children[child] 
-
-            if not childNode.children:
-                del parentNode.children[child]
-
-
-
 
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         
         root = TrieNode() 
-        
+
         for w in words:
-            root.addWord(w) 
+            root.add_word(w) 
 
         rows = len(board) 
         cols = len(board[0])
 
-        visit = set()
+        visit = set() 
         ans = set()
-        def dfs(r, c, node, pref):
-            if (min(r, c) < 0 or r == rows or c == cols or 
-                board[r][c] not in node.children or (r, c) in visit
-            ):
+
+        def dfs(r, c, prefix, node):
+            if min(r, c) < 0 or r == rows or c == cols or (r, c) in visit: 
                 return 
 
-            pref += board[r][c]
+            if board[r][c] not in node.children:
+                return 
 
-            node = node.children[board[r][c]]
+
+            visit.add((r, c)) 
+
+            prefix += board[r][c] 
+            node = node.children[board[r][c]] 
+            
             if node.word:
-                ans.add(pref) 
-                root.pruneWord(pref)
+                ans.add(prefix) 
 
-            visit.add((r, c))
-            dfs(r + 1, c, node, pref)
-            dfs(r - 1, c, node, pref)
-            dfs(r, c + 1, node, pref)
-            dfs(r, c - 1, node, pref) 
-            visit.remove((r,c))
-            return
+            dfs(r + 1, c, prefix, node)
+            dfs(r - 1 , c, prefix, node)
+            dfs(r, c + 1, prefix, node)
+            dfs(r, c - 1, prefix, node)
+
+            visit.remove((r, c)) 
+
+
 
 
 
         for r in range(rows):
             for c in range(cols):
-                dfs(r, c, root, "")
+                dfs(r, c, "", root)
 
-        
-
+    
         return list(ans)
