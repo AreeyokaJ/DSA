681453331d2266b836dda18656ac954cfdf3bc2d
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2026-02-12 17:00:15 -0500

    Time: 0 ms (100.00%) | Memory: 21.5 MB (7.96%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.py
+++ 261-graph-valid-tree/graph-valid-tree.py
@@ -1,29 +1,30 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        adj  = {i:[] for i in range(n)}
+        
+        adj = {i: [] for i in range(n)}
 
         for n1, n2 in edges:
             adj[n1].append(n2)
-            adj[n2].append(n1) 
+            adj[n2].append(n1)
 
-        
-        visit = set()
-
-        def dfs(node, prev):
-            if node in visit:
+        cycle = set()
+        def dfs(root, prev):
+            if root in cycle:
                 return False
 
-            visit.add(node)
-
-            for nei in adj[node]:
-                if nei == prev:
-                    continue
-                
-                if not dfs(nei, node):
-                    return False
             
+            cycle.add(root)
+            
+            for nei in adj[root]:
+                if nei == prev:
+                    continue 
 
-            return True
-        
+                if not dfs(nei, root):
+                    return False 
+                
+            return True 
+
     
-        return dfs(0, -1) and len(visit) == n 
+        return dfs(0, -1) and len(cycle) == n
+
+        
