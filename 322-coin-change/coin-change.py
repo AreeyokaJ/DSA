f0dcea9416f63db59fe571863a36e8dfa7c3ba54
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2026-02-18 18:37:04 -0500

    Time: 519 ms (67.72%) | Memory: 19.8 MB (36.97%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -6,8 +6,9 @@ class Solution:
 
 
         for a in range(1, amount + 1):
-            for c in coins:
-                if a - c >= 0:
-                    dp[a] = min(dp[a], dp[a - c] + 1)
 
-        return dp[amount] if dp[amount] != amount + 1 else -1
+            for c in coins:
+                if a - c >= 0: 
+                    dp[a] = min(dp[a], 1 + dp[a -c])
+
+        return dp[amount] if dp[amount] < amount + 1 else -1
