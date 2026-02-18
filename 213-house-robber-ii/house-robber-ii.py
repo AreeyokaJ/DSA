cacb81ed68e13f7255a95abb562be751bfe9c531
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2026-02-18 18:59:54 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (68.08%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,15 +1,23 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-    
-        return max(nums[0], self.helper(nums[:len(nums) - 1]), self.helper(nums[1:]))
+       return max(nums[0], self.take_money(nums[1:]), self.take_money(nums[:len(nums) -1]))
 
-    def helper(self, nums):
-        
+    
+    def take_money(self, nums):
+
         rob1, rob2 = 0, 0 
 
-        for n in nums:
+
+        for num in nums: 
             temp = rob2 
-            rob2 = max(rob1 + n, rob2)
+
+            rob2 = max(rob1 + num, rob2) 
             rob1 = temp 
-        
+
         return rob2
+
+
+
+
+
+
