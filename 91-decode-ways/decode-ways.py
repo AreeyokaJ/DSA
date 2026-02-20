d874669f1fccbd32676ab894d6a551eace9b1916
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2026-02-20 18:43:36 -0500

    Time: 0 ms (100.00%) | Memory: 20 MB (5.98%) - LeetSync

---
+++ 91-decode-ways/decode-ways.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        valid = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18",
+                    "19", "20", "21", "22", "23", "24", "25", "26"}
+
+
+        dp = {len(s): 1} 
+
+
+        def dfs(i):
+            if i in dp:
+                return dp[i] 
+            
+            if i > len(s):
+                return 0
+
+            if s[i] == "0":
+                return 0 
+
+            res = dfs(i + 1)
+
+            if s[i: i + 2] in valid:
+                res += dfs(i+2) 
+            
+            dp[i] = res 
+            return dp[i]
+
+        return dfs(0)
