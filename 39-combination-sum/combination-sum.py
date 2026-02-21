1e14ff9cbbc2543785c388599a39c182dcbb012f
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2026-02-21 08:19:47 -0500

    Time: 7 ms (77.05%) | Memory: 19.7 MB (28.38%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,24 +1,23 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         
-        ans = []
+
         stack = []
+        ans = []
 
         def dfs(total, j):
+            if total == target: 
+                ans.append(stack.copy()) 
+                return 
 
-            if total >= target:
-                if total == target:
-                    ans.append(stack.copy())
+            if total > target:
                 return 
 
             
             for i in range(j, len(candidates)):
-                stack.append(candidates[i]) 
+                stack.append(candidates[i])
                 dfs(total + candidates[i], i)
-                stack.pop()
-            
-
-            return
-
-        dfs(0, 0)
+                stack.pop() 
+            return 
+        dfs(0,0)
         return ans
