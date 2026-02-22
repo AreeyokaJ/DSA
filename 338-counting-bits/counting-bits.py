08335bf0acad3be90395476bc9c26b3076c5a687
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2026-02-22 14:38:59 -0500

    Time: 107 ms (5.04%) | Memory: 20.5 MB (18.12%) - LeetSync

---
+++ 338-counting-bits/counting-bits.py
@@ -0,0 +1,24 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+  
+        ans = []
+        for i in range(n + 1):
+            ans.append(self.hammer(i))
+
+        return ans
+
+
+
+
+    def hammer(self, n):
+        
+        res = 0 
+
+        for i in range(32): 
+            if n & 1 == 1: 
+                res += 1 
+
+            n = n >> 1 
+
+        return res
+    
