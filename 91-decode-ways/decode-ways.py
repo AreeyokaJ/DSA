f4d497a0dcf34a815ec80ce3e0b631d3d9032acd
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2026-02-22 13:57:16 -0500

    Time: 0 ms (100.00%) | Memory: 19.5 MB (32.60%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -5,24 +5,37 @@ class Solution:
 
         dp = {len(s):1} 
 
-        def dfs(i):
-            if i > len(s):
-                return 0
-            
-            if i in dp:
-                return dp[i]
-
-            
+        for i in range(len(s) -1, -1, -1):
             if s[i] == "0":
-                return 0 
+                dp[i] = 0 
+            else:
+                dp[i] = dp[i + 1] 
 
-            dp[i] = dfs(i + 1) 
-            res = dp[i]
+            
+            if i < len(s) - 1 and s[i:i +2] in valid:
+                dp[i] += dp[i + 2] 
+        
+        return dp[0]
 
-            if s[i:i + 2] in valid:
-                res += dfs(i + 2) 
-                dp[i] = res
+
+        # def dfs(i):
+        #     if i > len(s):
+        #         return 0
+            
+        #     if i in dp:
+        #         return dp[i]
+
+            
+        #     if s[i] == "0":
+        #         return 0 
+
+        #     dp[i] = dfs(i + 1) 
+        #     res = dp[i]
+
+        #     if s[i:i + 2] in valid:
+        #         res += dfs(i + 2) 
+        #         dp[i] = res
                 
-            return res 
+        #     return res 
 
-        return dfs(0)
+        # return dfs(0)
