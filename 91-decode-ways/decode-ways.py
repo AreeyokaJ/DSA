55b6b811c4a24e8f6cf6483a673776138b961d65
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2026-02-24 19:17:29 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (75.84%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,41 +1,19 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        valid = {"10", "11", "12", "13", "14", "15", "16", "17", "18",
-                    "19", "20", "21", "22", "23", "24", "25", "26"}
+        
+        valid = {"10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "26", "25"}
 
-        dp = {len(s):1} 
-
+        dp = {len(s): 1}
+        
         for i in range(len(s) -1, -1, -1):
+
             if s[i] == "0":
                 dp[i] = 0 
             else:
                 dp[i] = dp[i + 1] 
 
-            
-            if i < len(s) - 1 and s[i:i +2] in valid:
-                dp[i] += dp[i + 2] 
+            if i < len(s) - 1 and s[i: i +2] in valid:
+                dp[i] += dp[i+2]
+
         
         return dp[0]
-
-
-        # def dfs(i):
-        #     if i > len(s):
-        #         return 0
-            
-        #     if i in dp:
-        #         return dp[i]
-
-            
-        #     if s[i] == "0":
-        #         return 0 
-
-        #     dp[i] = dfs(i + 1) 
-        #     res = dp[i]
-
-        #     if s[i:i + 2] in valid:
-        #         res += dfs(i + 2) 
-        #         dp[i] = res
-                
-        #     return res 
-
-        # return dfs(0)
