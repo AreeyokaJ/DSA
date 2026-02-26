0075c24ff18c73c54e3719e0ab164bbe0f04223c
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2026-02-25 21:40:46 -0500

    Time: 59 ms (86.36%) | Memory: 19.8 MB (64.27%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,66 +1,38 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(t) > len(s):
+
+       
+        if t == "": 
             return ""
 
-        #create a frequency map of t
-        freqT = {}
-        for char in t:
-            freqT[char] = freqT.get(char, 0) + 1
 
-        #counter alg  ^^^^^^
+        countT = defaultdict(int)
 
-        l,r = 0,0 
+        for c in t: 
+            countT[c] += 1 
         
-        min_length = len(s)
-        res = "" 
-        
-        while r < len(s): 
-        
-            if s[r] in freqT:
-                freqT[s[r]] -= 1 
-            #while(current window not valid adjust r until window is valid)
-            while (not self.isValidWindow(freqT)) and r < len(s):
-                r += 1
 
-                if r < len(s) and s[r] in freqT:
-                    freqT[s[r]] -= 1 
+        have, need = 0, len(countT) 
+        resLen = float("inf")
+        l = 0 
+        res = [-1, -1]
+        window = defaultdict(int)
+        for r in range(len(s)): 
+
+            window[s[r]] += 1 
+
+            if s[r] in countT and window[s[r]] == countT[s[r]]: 
+                have += 1 
+
             
-            curr_length = (r - l) + 1 
-        
-            #if current_window_length < minlength: 
-            if curr_length < min_length:
+            while have == need: 
+                if (r -l + 1) < resLen:
+                    res = [l, r]
+                    resLen = r- l + 1 
                 
-                #update min_length to be min(current window, min_length)
-                min_length = curr_length 
-            
-                #update result substring to be substring of s (l, r)
-                res = s[l:r+1]
+                window[s[l]] -= 1 
+                if window[s[l]] < countT[s[l]]:
+                    have -= 1 
 
-
-            #while current_window is valid 
-            while(self.isValidWindow(freqT)) and l < len(s):
-                curr_length = (r - l) + 1 
-                
-                #if current_window_length < minlength: 
-                if curr_length <= min_length:
-                    #update min_length 
-                    min_length = curr_length 
-
-                    #update result substring 
-                    res = s[l:r+1]
-
-                if s[l] in freqT:
-                    freqT[s[l]] += 1 
-
-                l += 1
-
-            r += 1 
-
-        return res
-        
-    def isValidWindow(self, f_map):
-        for key in f_map:
-            if f_map[key] > 0:
-                return False 
-        return True 
+                l += 1 
+        return s[res[0]:res[1] + 1]
