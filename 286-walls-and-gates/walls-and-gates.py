509300794e304bc0183b0ec43080cba7a110944a
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2026-02-25 22:57:45 -0500

    Time: 37 ms (56.49%) | Memory: 22.1 MB (67.86%) - LeetSync

--- 286-walls-and-gates/walls-and-gates.py
+++ 286-walls-and-gates/walls-and-gates.py
@@ -4,47 +4,36 @@ class Solution:
         Do not return anything, modify rooms in-place instead.
         """
         
-        #if the room != inf do nothing
-        def add_rooms(r, c):
-            
-            if min(r, c) < 0 or r == rows or c == cols:
-                return 
-            
-            if rooms[r][c] != 2147483647:
-                return 
 
-            queue.append((r, c))
-            rooms[r][c] = d 
-
-
-        
-        queue = deque()
-        
-        rows = len(rooms)
+        rows = len(rooms) 
         cols = len(rooms[0]) 
+        queue = deque()
+        neighbors = [[0, 1], [1, 0], [-1, 0], [0, -1]]
+
 
         for r in range(rows):
-            for c in range(cols):
+            for c in range(cols): 
                 if rooms[r][c] == 0:
                     queue.append((r, c)) 
-        
 
-        #keep track of the distance
-        d = 1
-
-        #perform bfs 
+        d = 1 
         while queue:
 
             for i in range(len(queue)):
+
                 r, c = queue.popleft()
-                add_rooms(r + 1, c)
-                add_rooms(r - 1, c)
-                add_rooms(r, c + 1) 
-                add_rooms(r, c - 1)
 
-    
-            d += 1 
+                for dr, dc in neighbors: 
+                    nr = dr + r 
+                    nc = dc + c 
 
+                    if min (nr, nc) < 0 or nr == rows or nc == cols: 
+                        continue 
+                    
+                    if rooms[nr][nc] != 2147483647:
+                        continue 
 
+                    rooms[nr][nc] = d
+                    queue.append((nr, nc))
 
-        #update distance after each round
+            d +=1 
