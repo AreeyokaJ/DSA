03c9295a381cd0ed5da4c5a80ed9b2b30a632ffd
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2026-02-26 19:46:55 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (37.87%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -4,11 +4,12 @@ class Solution:
 
         row = [1] * n 
 
-        for i in range(m - 1): 
+        for i in range(m - 1):
             newRow = [1] * n 
-            for j in range(n  - 2, -1, -1):
-                newRow[j] = newRow[j + 1] + row[j]
+
+            for j in range(n - 2, -1, -1): 
+                newRow[j] = newRow[j+ 1] + row[j] 
             
             row = newRow 
-        
+
         return row[0]
