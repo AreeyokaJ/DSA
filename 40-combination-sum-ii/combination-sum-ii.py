df8b8c1da30f406826dc2e2ff1a11a4dff151151
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2026-03-08 23:26:11 -0400

    Time: 21 ms (47.05%) | Memory: 19.5 MB (88.65%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+
+
+        candidates.sort() 
+
+
+        stack = [] 
+        ans = []
+
+        def dfs(j, total): 
+            if total > target:
+                return
+
+            if total == target: 
+                ans.append(stack.copy())
+                return 
+            
+
+            for i in range(j, len(candidates)):
+                if i >j and candidates[i] == candidates[i - 1]:
+                    continue 
+
+                stack.append(candidates[i])
+                dfs(i + 1, total + candidates[i]) 
+                stack.pop()
+
+        dfs(0, 0)
+
+        return ans 
