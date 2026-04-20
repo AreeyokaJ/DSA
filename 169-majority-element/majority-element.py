5e86cb6ea70bb298624ce8adb955e8bde375c59b
Areeyoka John <97417918+AreeyokaJ@users.noreply.github.com>
2026-04-20 19:53:30 -0400

    Time: 8 ms (40.23%) | Memory: 21.3 MB (25.45%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -2,16 +2,14 @@ from collections import defaultdict
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
 
-        freq = defaultdict(int)
+        freq = defaultdict(int) 
 
         for num in nums:
             freq[num] += 1 
 
-        n = len(nums)
-
-        for num in freq: 
-            if freq[num] > n //2: 
+        
+        for num in freq:
+            if freq[num] > len(nums) / 2:
                 return num
-
 
         
